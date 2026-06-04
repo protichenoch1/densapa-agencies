8cd43e47be1f09a0f2e94993a8e57b02efe046ba
protichenoch1 <protichenoch1@gmail.com>
2026-06-04 12:29:13 +0300

    Create layout.js with metadata and RootLayout component

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "DENSAPAL AGENCIES",
+  description: "Investment Platform"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
