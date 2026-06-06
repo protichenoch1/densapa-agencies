aae7b471a572ccc9a592f1c9fcafb5fef490219a
protichenoch1 <protichenoch1@gmail.com>
2026-06-06 09:49:00 +0300

    Add BottomNav component with navigation links

---
+++ components/BottomNav.js
@@ -0,0 +1,27 @@
+export default function BottomNav() {
+  return (
+    <div className="bottom-nav">
+
+      <a href="/">
+        <span>🏠</span>
+        Home
+      </a>
+
+      <a href="/team">
+        <span>👥</span>
+        Team
+      </a>
+
+      <a href="/wallet">
+        <span>💳</span>
+        Wallet
+      </a>
+
+      <a href="/account">
+        <span>👤</span>
+        Account
+      </a>
+
+    </div>
+  );
+}
