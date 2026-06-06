f395b8ef17d91231f1475808823009511bac84a1
protichenoch1 <protichenoch1@gmail.com>
2026-06-06 12:48:31 +0300

    Add modal display logic for selected plan

--- app/page.js
+++ app/page.js
@@ -463,6 +463,22 @@ return (
       </div>
 
 
+      {showModal && selectedPlan && (
+  <div
+    style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      background: "rgba(0,0,0,0.5)",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      zIndex: 9999
+    }}
+  >
+    <div
       style={{
         background: "#fff",
         padding: "20px",
