c4e1a053ccd3c0b80342af63fb0e7b63f709f111
protichenoch1 <protichenoch1@gmail.com>
2026-06-09 18:26:05 +0300

    Change query to use maybeSingle for login

--- app/login/page.js
+++ app/login/page.js
@@ -32,7 +32,7 @@ export default function Login() {
       .select("*")
       .eq("phone_number", phoneNumber)
       .eq("pin", pin)
-      .single();
+      .maybeSingle();
 
     setLoading(false);
 
