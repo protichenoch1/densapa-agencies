4252f09047a30985d32a47e124ae76e59dc13c0f
protichenoch1 <protichenoch1@gmail.com>
2026-06-11 07:13:54 +0300

    Fix phone_number reference in withdraw page

--- app/withdraw/page.js
+++ app/withdraw/page.js
@@ -40,7 +40,7 @@ const [loading, setLoading] = useState(false);
       {
         user_id: user.id,
         amount: Number(amount),
-        phone_number: user phone_number,
+        phone_number: user.phone_number,
       },
     ]);
 
