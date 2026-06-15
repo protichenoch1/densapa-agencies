f2ac4ed1405c1e8aca47587b43c772a50dc614c6
protichenoch1 <protichenoch1@gmail.com>
2026-06-15 13:50:06 +0300

    Remove Toaster from RootLayout component
    Removed Toaster component from RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "DENSAPAL AGENCIES",
@@ -10,33 +9,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-  {children}
-  <Toaster
-  position="top-center"
-  toastOptions={{
-    duration: 3000,
-    style: {
-      background: "#fff",
-      color: "#0A3D91",
-      borderRadius: "12px",
-      padding: "16px",
-      fontWeight: "bold",
-    },
-    success: {
-      iconTheme: {
-        primary: "#28a745",
-        secondary: "#fff",
-      },
-    },
-    error: {
-      iconTheme: {
-        primary: "#dc3545",
-        secondary: "#fff",
-      },
-    },
-  }}
-/>
-</body>
+        {children}
+      </body>
     </html>
   );
 }
