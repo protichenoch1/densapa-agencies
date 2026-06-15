2f1e73d4337601f6f61f274182fad8f26dcc4d29
protichenoch1 <protichenoch1@gmail.com>
2026-06-15 13:18:19 +0300

    Include Toaster in RootLayout component
    Added Toaster component to RootLayout for notifications.

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+  {children}
+  <Toaster position="top-center" />
+</body>
     </html>
   );
 }
